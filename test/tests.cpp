d85b069a616179d25b97caea8ea9438c77400a5a
karlajusten <karlapm2@gmail.com>
2017-10-16 10:55:01 -0200

    ..

--- test/tests.cpp
+++ test/tests.cpp
@@ -22,21 +22,79 @@
 	temp = [0,50] em graus Celsius; -1 = error;
 **/
 
-TEST(Example,AutomaticWindow) {
-
 	Sensor sensor;
 	Configuration config;
 	Brain brain;
 	
-
 	sensor.setConditionstoSimulate(1, 0, 20, 1); // led, chuva, temperatura, luz
 	config.setAutomaticDay(true);
 	config.setAutomaticNight(true);
 	config.setMinTemp(17);
 	config.setMaxTemp(24);
 	sensor.openWindow();
-	//chamar o método do brain que irá fazer o processamento de automatização
+
+TEST(Linha1,AutomaticWindow) {
+
 	brain.test();
 	EXPECT_EQ(true, sensor.isOpen());
 }
+TEST(Linha2,AutomaticWindow) {
 
+	sensor.setConditionstoSimulate(1, 0, 24, 1); // led, chuva, temperatura, luz
+	brain.test();
+	EXPECT_EQ(true, sensor.isOpen());
+}
+TEST(Linha3,AutomaticWindow) {
+
+	sensor.setConditionstoSimulate(1, 0, 25, 1); // led, chuva, temperatura, luz
+	brain.test();
+	EXPECT_EQ(true, sensor.isOpen());
+}
+TEST(Linha4,AutomaticWindow) {
+
+	sensor.setConditionstoSimulate(1, 0, 23, 1); // led, chuva, temperatura, luz
+	brain.test();
+	EXPECT_EQ(true, sensor.isOpen());
+}
+TEST(Linha5,AutomaticWindow) {
+
+	sensor.setConditionstoSimulate(1, 1, 21, 1); // led, chuva, temperatura, luz
+	brain.test();
+	EXPECT_EQ(false, sensor.isOpen());
+}
+TEST(Linha6,AutomaticWindow) {
+
+	sensor.setConditionstoSimulate(0, 1, 20, 1); // led, chuva, temperatura, luz
+	brain.test();
+	EXPECT_EQ(false, sensor.isOpen());
+}
+TEST(Linha7,AutomaticWindow) {
+
+	sensor.setConditionstoSimulate(0, 0,19, 0); // led, chuva, temperatura, luz
+	brain.test();
+	EXPECT_EQ(false, sensor.isOpen());
+}
+TEST(Linha8,AutomaticWindow) {
+
+	sensor.setConditionstoSimulate(0, 0, 18, 0); // led, chuva, temperatura, luz
+	brain.test();
+	EXPECT_EQ(false, sensor.isOpen());
+}
+TEST(Linha9,AutomaticWindow) {
+
+	sensor.setConditionstoSimulate(0, 0,17, 0); // led, chuva, temperatura, luz
+	brain.test();
+	EXPECT_EQ(false, sensor.isOpen());
+}
+TEST(Linha10,AutomaticWindow) {
+
+	sensor.setConditionstoSimulate(0, 0,16, 0); // led, chuva, temperatura, luz
+	brain.test();
+	EXPECT_EQ(false, sensor.isOpen());
+}
+TEST(Linha11,AutomaticWindow) {
+
+	sensor.setConditionstoSimulate(0, 0,25, 0); // led, chuva, temperatura, luz
+	brain.test();
+	EXPECT_EQ(true, sensor.isOpen());
+}
